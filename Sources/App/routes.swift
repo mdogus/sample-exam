5680bd19d5032f46a599d9d9f5434636d2b5fbac
Mustafa Doğuş <mustafadogus45@gmail.com>
2019-12-28 11:40:03 +0300

    replica0

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,10 +1,21 @@
 import Vapor
+import Leaf
+import FluentPostgreSQL
 
 struct DisplayAllQuestionsContext: Encodable {
     var allQuestions: [Question]
 }
 struct IndexContext: Encodable {
     var questions: [Question]?
+}
+struct IndexContextForUser: Encodable {
+    var users: [User]?
+}
+struct DeleteRequestForUser: Content {
+    var userId: Int
+}
+struct DeleteRequestForTurkishQuestion: Content {
+    var questionId: Int
 }
 
 /// Register your application's routes here.
@@ -40,6 +51,30 @@ public func routes(_ router: Router) throws {
             .transform(to: request.redirect(to: "/admin"))
     }
     
+    //Admin: New user
+    router.get("admin/new-user") { request -> Future<View> in
+        return try request.view().render("admin-new-user")
+    }
+    router.post(User.self, at: "/admin/new-user") { request, user -> Future<Response> in
+        return user.save(on: request)
+            .transform(to: request.redirect(to: "/admin/all-users"))
+    }
+    //Users: All users
+    router.get("/admin/all-users") { request -> Future<View> in
+        return User.query(on: request).all()
+            .flatMap(to: View.self) { allUsers in
+                let userData = allUsers.isEmpty ? nil : allUsers
+                let context = IndexContextForUser(users: userData)
+                return try request.view().render("admin-users-all", context)
+        }
+    }
+    //Deleting a user
+    router.post(DeleteRequestForUser.self, at: "/delete-user") { request, deleteRequest -> Future<Response> in
+        return User.query(on: request).filter(\.id == deleteRequest.userId)
+            .first().unwrap(or: Abort(.badRequest)).delete(on: request)
+            .transform(to: request.redirect(to: "/admin/all-users"))
+    }
+    
     //Admin-Tests: Turkish
     let tests = router.grouped("admin/tests")
     tests.get("/turkish") { request -> Future<View> in
@@ -49,6 +84,12 @@ public func routes(_ router: Router) throws {
                 let context = IndexContext(questions: questionData)
                 return try request.view().render("admin-test-turkish", context)
         }
+    }
+    //Admin: Deleting a question from Turkish test
+    router.post(DeleteRequestForTurkishQuestion.self, at: "/delete-turkish-question") { request, deleteRequest -> Future<Response> in
+        return Question.query(on: request).filter(\.id == deleteRequest.questionId)
+            .first().unwrap(or: Abort(.badRequest)).delete(on: request)
+            .transform(to: request.redirect(to: "/admin/tests/turkish"))
     }
     
     //Test interface: "sinav"
